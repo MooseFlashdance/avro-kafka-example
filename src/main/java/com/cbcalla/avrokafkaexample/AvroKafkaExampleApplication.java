ca2ce7b04ef546a0e01c4c383c6acdad2904861e
Corey Callaway <mooseflashdance@MacBook-Pro.attlocal.net>
2020-08-15 18:51:03 -0500

    add comment on offset behavior

--- src/main/java/com/cbcalla/avrokafkaexample/AvroKafkaExampleApplication.java
+++ src/main/java/com/cbcalla/avrokafkaexample/AvroKafkaExampleApplication.java
@@ -92,6 +92,9 @@ public class AvroKafkaExampleApplication implements CommandLineRunner {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP);
+
+        // The offset isn't getting saved in my dev setup for some reason
+        // so I just get all the messages for testing purposes
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
